545c7ad7a288b202cf44f1cb05c6ef2319693648
mohamed <mohamed@utopios.net>
2024-10-25 09:26:30 +0200

    add demo collection + debut stream

--- src/main/java/org/example/stream/Demo1.java
+++ src/main/java/org/example/stream/Demo1.java
@@ -2,6 +2,7 @@ package org.example.stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Demo1 {
@@ -10,8 +11,7 @@ public class Demo1 {
 
         List<String> list = Arrays.asList("toto",
                 "aurelien", "sylvain","robert",
-                "adrien","aurelie","christophe","dominique","mohamed","walter");
-
+                "adrien","aurelie","christophe","dominique","mohamed","walter", "mohamed");
 
         System.out.println("Filter : filtre : ");
         list.stream()
@@ -19,6 +19,31 @@ public class Demo1 {
                 .forEach(System.out::println);
 
 
+
+        System.out.println("Compter : count ");
+
+        System.out.println("Le nombre de prenom dans ma liste est de : " + list.stream()
+                .filter(p -> p.startsWith("a"))
+                .count());
+
+        System.out.println("Distinct : ");
+
+        list.stream().distinct().forEach(System.out::println);
+
+        System.out.println("limit : ");
+
+        list.stream().limit(3).forEach(System.out::println);
+
+        System.out.println("sort : ");
+
+        list.stream().sorted().forEach(System.out::println);
+
+        System.out.println("inverse sort : ");
+
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+
+        // manipulation avec Objet
 
 
 
