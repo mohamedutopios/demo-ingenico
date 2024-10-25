c67202cec4900c4fa54aae2c451b3495dab9526a
mohamed <mohamed@utopios.net>
2024-10-25 15:34:41 +0200

    add demo IO

--- src/main/java/org/example/io/FileReadExample.java
+++ src/main/java/org/example/io/FileReadExample.java
@@ -1,7 +1,23 @@
 package org.example.io;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReadExample {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/toto.txt"));
+            String line;
+            while((line = bufferedReader.readLine()) != null ){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());;
+        }
 
     }
 }
